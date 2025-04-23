1900f9a5f76f2df2453f9ff5e2d595dfeeb2b742
Anderson Godoy <anderson.a.godoy@gmail.com>
2025-04-23 16:24:41 -0300

    adiciona cookie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Post, Request, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -8,7 +9,17 @@ export class AuthController {
 
   @UseGuards(AuthGuard('local'))
   @Post('login')
-  login(@Request() req) {
-    return this.authService.login(req);
+  login(@Request() req, @Res({ passthrough: true }) res: Response) {
+    const token = this.authService.login(req); // Supondo que retorne o JWT
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
+      maxAge: 60 * 60 * 24 * 1000, // 1 day
+      path: '/',
+    });
+
+    return { message: 'Login successful' };
   }
 }
