b8fa0968083a8f67ca4b91d0a1af04c0dec7c35b
Anderson Godoy <anderson.a.godoy@gmail.com>
2025-04-24 12:20:39 -0300

    altera controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,11 +11,17 @@ export class AuthController {
   @Post('login')
   login(@Request() req, @Res({ passthrough: true }) res: Response) {
     const { access_token } = this.authService.login(req.user); // Supondo que retorne o JWT
-
+    res.setHeader(
+      'Access-Control-Allow-Origin',
+      typeof req.headers.origin === 'string'
+        ? (req.headers.origin as string)
+        : '*',
+    );
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.cookie('token', access_token, {
       httpOnly: true,
       secure: false,
-      sameSite: 'none',
+      sameSite: 'lax',
       maxAge: 60 * 60 * 24 * 1000, // 1 day
       path: '/',
     });
